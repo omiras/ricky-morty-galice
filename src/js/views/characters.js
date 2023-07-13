b414206f99aa350d30ded6f5fe297d19df7b89e5
Oscar Miras <oscar.miras@gmail.com>
2023-07-13 16:16:24 +0000

    NEw static route get all characters

--- src/js/views/characters.js
+++ src/js/views/characters.js
@@ -1,9 +1,38 @@
-import  React from "react";
+import React, { useEffect, useState } from "react";
+
+/**
+ * 1. Declarar una variable de estado para albergar todos los characters
+ * 2. usar useEffect para hacer una única petición a la API https://rickandmortyapi.com/api/character y traernos todos los personajes de la serie
+ * 3. Iterar la variable de estado para mostrar  la imagen y nombre de cada uno de los personajes 
+ * Corregimos a las 18h
+ */
 
 export function Characters() {
+
+    const [characters, setCharacters] = useState([]);
+
+    const getAllCharacters = () => {
+        fetch('https://rickandmortyapi.com/api/character')
+            .then(res => res.json())
+            .then(data => setCharacters(data.results));
+    }
+
+    useEffect(() => {
+        getAllCharacters(); // <--- implementame !!
+    }, [])
+
     return (
-    <div>
-        SOY LA PAG CHARACTERS
-    </div>
+        <div className="text-center">
+            <h1>All Characters</h1>
+            <div className="container">
+                <div className="row row-cols-3">
+                    {characters.map(c => <div key={c.id}>
+                        <img src={c.image}></img>
+                        <p>{c.name}</p>
+                    </div>)}
+                </div>
+            </div>
+
+        </div>
     );
 }
