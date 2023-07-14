1f2e3b88ec32c83df93008a692d67eb47d0ddfe6
Oscar Miras <oscar.miras@gmail.com>
2023-07-14 17:38:26 +0200

    paginacion, falta establecer limite superior

--- src/js/views/characters.js
+++ src/js/views/characters.js
@@ -11,21 +11,26 @@ import { Link } from "react-router-dom";
 export function Characters() {
 
     const [characters, setCharacters] = useState([]);
+    const [page, setPage] = useState(1);
 
     const getAllCharacters = () => {
-        fetch('https://rickandmortyapi.com/api/character')
+        fetch('https://rickandmortyapi.com/api/character?page=' + page)
             .then(res => res.json())
             .then(data => setCharacters(data.results));
     }
 
     useEffect(() => {
         getAllCharacters(); // <--- implementame !!
-    }, [])
+    }, [page])
 
     return (
         <div className="text-center">
             <h1>All Characters</h1>
             <div className="container">
+                <div className="d-flex justify-content-between mb-2">
+                    {page !== 1 && <button onClick={() => setPage(page - 1)} className="btn btn-primary">Prev</button>}
+                    <button onClick={() => setPage(page + 1)} className="btn btn-primary">Next</button>
+                </div>
                 <div className="row row-cols-3">
                     {characters.map(c => <Link key={c.id} to={"/character/" + c.id}><div >
                         <img src={c.image}></img>
